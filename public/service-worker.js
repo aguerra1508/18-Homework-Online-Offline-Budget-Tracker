53156dde5648c6cb3d44b7a3621ceed36fe63689
Analee Guerra <aguerra1508@gmail.com>
2020-11-24 15:00:27 -0600

    added db.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,38 +1,41 @@
+const { static } = require("express");
+
+// Testing if service worker is being read
 console.log("Hello from your service worker!");
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+// Cache variables
+const staticCache = "static-cache-v2";
+const dataCache = "data-cache-v1";
 
-const iconSizes = ["192","512"];
-const iconFiles = iconSizes.map(
-  (size) => `/public/images/icons/icon-${size}x${size}.png`
-);
-
+// Files to cache
 const staticFilesToPreCache = [
   "/",
   "/index.js",
-  "/manifest.webmanifest",
-].concat(iconFiles);
+  "/index.html",
+	"/manifest.webmanifest",
+	"/styles.css",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png"
+];
 
-// install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
+// Install
+self.addEventListener("install", (e) => {
+  console.log("Service Worker Install");
+  e.waitUntil(
+    caches.open(staticCache).then((cache) => {
+          console.log("Service Worker Caching all: app shell and content");
       return cache.addAll(staticFilesToPreCache);
     })
   );
-
-  self.skipWaiting();
 });
 
-// activate
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
+// Activate
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          if (key !== staticCache && key !== dataCache) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
           }
@@ -40,41 +43,21 @@ self.addEventListener("activate", function(evt) {
       );
     })
   );
-
   self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", function(evt) {
-  const {url} = evt.request;
-  if (url.includes("/api/transaction")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request, response.clone());
-            }
-
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
-    );
-  } else {
-    // respond from static cache, request is not for /api/*
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log("Service Worker Fetching resource: "+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(staticCache).then((cache) => {
+          console.log("Service Worker Caching new resource: "+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
         });
-      })
-    );
-  }
+      });
+    })
+  );
 });
-
-
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -7,22 +8,26 @@ const PORT = 3000;
 
 const app = express();
 
+// Middeleware
 app.use(logger("dev"));
 
+// Setting up express app
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+// Mongoose connection
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+// Routes
 app.use(require("./routes/api.js"));
 
+// Server
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
